060870cc0a2243254ee226d3730e72e07c254fc9
ashmithak20 <ashmithakumble99@gmail.com>
2022-03-23 10:46:58 +0530

    profile changes

--- delivery/profile.php
+++ delivery/profile.php
@@ -164,11 +164,11 @@
                     <h4 class="text-right">Profile Settings</h4>
                 </div>
                 <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="enter name" value=""></div>
+                    <div class="col-md-12"><label class="labels">Name</label><input type="text" class="form-control" placeholder="enter name" value=""></div>
                     
                 </div>
                 <div class="row mt-3">
-                    <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
+                    <div class="col-md-12"><label class="labels">Contact Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
                     <div class="col-md-12"><label class="labels">Address</label><input type="text" class="form-control" placeholder="enter address" value=""></div>
                    
                 </div>
