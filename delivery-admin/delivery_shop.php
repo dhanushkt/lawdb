45189162dad78b1a926f8aa4bec8806221e08279
dhanushkt <dhanushkt787@gmail.com>
2022-04-20 22:09:58 +0530

    delivery_shop page UI updates

--- delivery-admin/delivery_shop.php
+++ delivery-admin/delivery_shop.php
@@ -16,25 +16,31 @@
         <div class="container-fluid page-body-wrapper">
             <!-- sidebar.php contains partial:partials/_sidebar.html -->
             <?php include './assets/sidebar.php'; ?>
-			<div style="overflow-x: auto;">
-			<div class="row">
-               <div class="col-xl-6 mb-4">
-			   <span style="color:black;font-weight:900;">Shop id:SH006</span>
-				</div>
-                <div class="col-xl-6 mb-4">
-					<span style="color:black;float:right;font-weight:900;">Date:10-02-2022</span>  
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <!-- <div style="overflow-x: auto; width: 100%;"> -->
+                    <div class="row">
+                        <div class="col-lg-6 grid-margin stretch-card">
+                            <span style="color: black; font-weight: 900;">Shop id:SH006</span>
+                        </div>
+                        <div class="col-lg-6">
+                            <span style="color:black; float:right; font-weight:900;">Date:10-02-2022</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 grid-margin stretch-card">
+                            <span style="color:black;font-weight:900;">shop name:Ice cream shop</span>
+                        </div>
+                        <div class="col-lg-6">
+                            <span style="color:black; float:right; font-weight:900;">Date:10-02-2022</span>
+                        </div>
+                    </div>
+                    <!-- </div> -->
                 </div>
-			</div>
-			<div class="row">
-               <div class="col-xl-6 mb-4">
-			   <span style="color:black;font-weight:900;">shop name:Ice cream shop</span>
-				</div>
-                <div class="col-xl-6 mb-4">
-					<span style="color:black;float:right;font-weight:900;">Date:10-02-2022</span>  
-                </div>
-			</div>
-			</div>
-		</div>
+            </div>
+        </div>
+    </div>
     </div>
 </body>
+
 </html>
